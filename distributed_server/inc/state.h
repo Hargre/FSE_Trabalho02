973b9092f69860a5eafe78972a5be94c9a0a539d
Felipe Hargreaves <fhargreaves00@gmail.com>
2020-11-02 15:44:20 -0300

    Adiciona mecanismo para não enviar alarme repetidamente.

--- distributed_server/inc/state.h
+++ distributed_server/inc/state.h
@@ -1,6 +1,11 @@
 #ifndef STATE_H
 #define STATE_H
 
+typedef struct {
+    unsigned char state;
+    unsigned char alarm_activated;
+} GuardedDevice;
+
 struct HouseState {
     float temperature;
     float humidity;
@@ -10,14 +15,14 @@ struct HouseState {
     unsigned char lamp04;
     unsigned char air01;
     unsigned char air02;
-    unsigned char presence01;
-    unsigned char presence02;
-    unsigned char open01;
-    unsigned char open02;
-    unsigned char open03;
-    unsigned char open04;
-    unsigned char open05;
-    unsigned char open06;
+    GuardedDevice presence01;
+    GuardedDevice presence02;
+    GuardedDevice open01;
+    GuardedDevice open02;
+    GuardedDevice open03;
+    GuardedDevice open04;
+    GuardedDevice open05;
+    GuardedDevice open06;
 };
 
 struct HouseState init_state();
@@ -26,6 +31,8 @@ int get_presence_sensors_state(struct HouseState *state);
 int get_open_sensors_state(struct HouseState *state);
 void get_climate_state(struct HouseState *state);
 void get_air_lamps_state(struct HouseState *state);
+void init_guarded_device_state(GuardedDevice *device, int device_flag);
+int get_guarded_device_state(GuardedDevice *device, int device_flag);
 
 char *state_to_string(struct HouseState *state);
 
--- distributed_server/src/state.c
+++ distributed_server/src/state.c
@@ -14,14 +14,14 @@ struct HouseState init_state() {
     state.lamp04 = read_device(LAMP04);
     state.air01 = read_device(AIR01);
     state.air02 = read_device(AIR02);
-    state.presence01 = read_device(PRESENCE01);
-    state.presence02 = read_device(PRESENCE02);
-    state.open01 = read_device(OPEN01);
-    state.open02 = read_device(OPEN02);
-    state.open03 = read_device(OPEN03);
-    state.open04 = read_device(OPEN04);
-    state.open05 = read_device(OPEN05);
-    state.open06 = read_device(OPEN06);
+    init_guarded_device_state(&(state.presence01), PRESENCE01);
+    init_guarded_device_state(&(state.presence02), PRESENCE02);
+    init_guarded_device_state(&(state.open01), OPEN01);
+    init_guarded_device_state(&(state.open02), OPEN02);
+    init_guarded_device_state(&(state.open03), OPEN03);
+    init_guarded_device_state(&(state.open04), OPEN04);
+    init_guarded_device_state(&(state.open05), OPEN05);
+    init_guarded_device_state(&(state.open06), OPEN06);
 
     return state;
 }
@@ -40,20 +40,40 @@ void get_climate_state(struct HouseState *state) {
     get_sensor_data(&(state->temperature), &(state->humidity));
 }
 
+void init_guarded_device_state(GuardedDevice *device, int device_flag) {
+    device->alarm_activated = 0;
+    device->state = read_device(device_flag);
+}
+
+int get_guarded_device_state(GuardedDevice *device, int device_flag) {
+    device->state = read_device(device_flag);
+    if (device->state && !device->alarm_activated) {
+        device->alarm_activated = 1;
+        return 1;
+    } else if (!device->state && device->alarm_activated) {
+        device->alarm_activated = 0;
+    }
+    return 0;
+}
+
 int get_presence_sensors_state(struct HouseState *state) {
-    state->presence01 = read_device(PRESENCE01);
-    state->presence02 = read_device(PRESENCE02);
-    return (state->presence01 || state->presence02);
+    int trigger_alarm = 0;
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->presence01), PRESENCE01);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->presence02), PRESENCE02);
+
+    return trigger_alarm;
 }
 
 int get_open_sensors_state(struct HouseState *state) {
-    state->open01 = read_device(OPEN01);
-    state->open02 = read_device(OPEN02);
-    state->open03 = read_device(OPEN03);
-    state->open04 = read_device(OPEN04);
-    state->open05 = read_device(OPEN05);
-    state->open06 = read_device(OPEN06);
-    return (state->open01 || state->open02 || state->open03 || state->open04 || state->open05 || state->open06);
+    int trigger_alarm = 0;
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open01), OPEN01);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open02), OPEN02);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open03), OPEN03);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open04), OPEN04);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open05), OPEN05);
+    trigger_alarm = trigger_alarm || get_guarded_device_state(&(state->open06), OPEN06);
+
+    return trigger_alarm;
 }
 
 char *state_to_string(struct HouseState *state) {
